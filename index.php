389fb86e13e205dfb5d2dc3e16e3a63e40eb9e10
Carlos J Ramirez <carlosjramirez11@gmail.com>
2022-07-21 21:22:42 -0700

    Created the form and start styling

--- index.php
+++ index.php
@@ -5,12 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/3e848d41da.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
     <title>PHP Login</title>
 </head>
 
 <body>
     <h1>PHP LOGIN FORM</h1>
+    <div class="loginContainer">
+
+        <form action="">
+            <div class="logo">
+                <i class="fa-solid fa-meteor fa-2xl"></i>
+            </div>
+            <div class="inputContainer">
+                <i class="fa-solid fa-user-large"></i>
+                <input type="text" placeholder="User Name">
+            </div>
+            <div class="inputContainer">
+                <i class="fa-solid fa-lock"></i>
+                <input type="text" placeholder="Password">
+            </div>
+            <div class="buttonContainer">
+                <button>Log In</button>
+                <button>Sign Up</button>
+            </div>
+
+        </form>
+    </div>
+    <div class="loginFooter">Made with love and coffee =]</div>
 </body>
 
 </html>
